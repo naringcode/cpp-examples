fe157f149e180fc64ed27d91edb756f2a4b0146f
naringcode <naringcode@gmail.com>
2024-05-12 18:19:25 +0900

    template meta programming

---
+++ TemplateMeta/factorial.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <int N>
+struct Factorial
+{
+    static constexpr int val = N * Factorial<N - 1>::val;
+};
+
+// 템플릿 특수화
+template <>
+struct Factorial<0>
+{
+    static constexpr int val = 1;
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cout << Factorial<5>::val << '\n';
+    cout << Factorial<4>::val << '\n';
+    cout << Factorial<3>::val << '\n';
+    cout << Factorial<2>::val << '\n';
+    cout << Factorial<1>::val << '\n';
+
+    return 0;
+}
