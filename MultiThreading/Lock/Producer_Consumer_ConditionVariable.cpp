720aa6cb61a89ec2d6f9d8c49c54b2b17227c093
naringcode <naringcode@gmail.com>
2024-09-21 12:32:53 +0900

    Producer Consumer CV

--- MultiThreading/Lock/Producer_Consumer_ConditionVariable.cpp
+++ MultiThreading/Lock/Producer_Consumer_ConditionVariable.cpp
@@ -1,58 +1,88 @@
 #include <iostream>
 
-#include <thread>
-#include <mutex>
+#include <vector>
 #include <queue>
 
-std::mutex mtx;
-std::queue<int> q;
+#include <atomic>
+#include <mutex>
 
-// CV는 User-Level Object(커널 오브젝트가 아니다)
-std::condition_variable cv;
+using namespace std;
+
+atomic<bool> g_IsRunning;
+
+mutex mtx;
+condition_variable cv; // CV는 User-Level Object(커널 오브젝트가 아니다)
+
+queue<int> q;
 
 void Producer()
 {
-    using namespace std::chrono;
-
-    while (true)
+    while (g_IsRunning.load())
     {
-        // Produce
+        //
         {
-            std::unique_lock<std::mutex> lock(mtx);
+            unique_lock<mutex> lock(mtx);
 
-            q.push(100);
+            q.push(10000);
         }
 
-        cv.notify_one(); // Wait 상태인 스레드가 있으면 딱 하나만 깨운다.
+        cv.notify_one();
 
-        // 소비하는 쪽이 조금 더 빠르게 하기
-        std::this_thread::yield();
+        this_thread::sleep_for(500ms);
     }
+
+    cout << "End 1\n";
 }
 
 void Consumer()
 {
-    while (true)
+    while (g_IsRunning.load())
     {
-        std::unique_lock<std::mutex> lock(mtx);
+        unique_lock<mutex> lock(mtx);
 
-        cv.wait(lock, []() { return false == q.empty(); });
+        cv.wait(lock, []() { 
+            if (false == g_IsRunning.load())
+                return true;
 
-        // while (false == q.empty())
-        {
-            int data = q.front();
-            q.pop();
+            return q.empty() == false;
+        });
 
-            // q에 쌓인 데이터를 출력하게 변경
-            std::cout << q.size() << '\n';
-        }
+        // 종료 로직을 대비한 코드
+        if (false == g_IsRunning.load())
+            break;
+
+        // true면 실행
+        int val = q.front();
+        q.pop();
+
+        cout << val << '\n';
     }
+
+    cout << "End 2\n";
 }
 
 int main()
 {
-    std::thread th1(Producer);
-    std::thread th2(Consumer);
+    g_IsRunning = true;
+
+    thread th1{ Producer };
+    thread th2{ Consumer };
+
+    this_thread::sleep_for(5000ms);
+
+    //
+    // g_IsRunning.store(false);
+    // cv.notify_one();
+    {
+        // Lock을 걸고 값을 수정한 다음 notify_one()을 해야 안전한 느낌
+        // notify()를 거는 것과 공유 자원을 대상으로 lock을 잡고 푸는 것은 원자적이지 않다.
+        unique_lock<mutex> lock(mtx);
+
+        g_IsRunning.store(false);
+    }
+
+    cv.notify_one();
+    //
 
     th1.join();
     th2.join();
