b7d8208e212a78395eb6767e1c7bb949e2a16a17
naringcode <naringcode@gmail.com>
2025-01-22 16:37:51 +0900

    docs: more detail explanations when separating declarations and implementations

--- Modules/04_separate_declaration_and_definition_to_different_files/BasicModule_PrintImpl.cpp
+++ Modules/04_separate_declaration_and_definition_to_different_files/BasicModule_PrintImpl.cpp
@@ -18,5 +18,5 @@ void Print(const string& str)
 }
 
 // Private Module Fragment : Optional
-// export module 쪽이 아니기 때문에 사용 불가(Private Module Fragment는 인터페이스를 정의하는 쪽에서만 사용 가능)
+// Private Module Fragment는 주 모듈(Primary Module) 쪽에서만 사용 가능하다.
 // module: private;
