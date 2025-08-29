c4e676a5b8a4f8072cdb57ff6ffdea67c6048a06
naringcode <naringcode@gmail.com>
2025-08-29 12:00:09 +0900

    docs: correct misleading comments

--- Functional/implicitly_capture_this.cpp
+++ Functional/implicitly_capture_this.cpp
@@ -1,4 +1,4 @@
-// Update Date : 2025-01-14
+// Update Date : 2025-08-29
 // OS : Windows 10 64bit
 // Program : Visual Studio 2022, vscode(gcc-13.1.0)
 // Version : C++20
@@ -10,11 +10,36 @@
 
 using namespace std;
 
-// https://en.cppreference.com/w/cpp/language/explicit
-// The implicit capture of *this when the capture default is = is deprecated.(since C++20)
+// https://en.cppreference.com/w/cpp/language/lambda.html
+// The current object (*this) can be implicitly captured if either capture default is present. 
+// If implicitly captured, it is always captured by reference, even if the capture default is =.
+// The implicit capture of *this when the capture default is = is deprecated.(since C++20).
 
-// C++20 이후에는 deprecated 된 내용이지만 클래스 안에 람다식을 구성할 때 캡처 블록에 "="을 쓰면 "암묵적으로" this 포인터를 캡처한다.
-// !! 이는 인스턴스가 유효하지 않게 되었는데 람다를 호출하여 사용할 때 문제가 되는 부분임. !!
+// 인스턴스화된 클래스의 멤버 함수에서 람다식을 구성할 때 [=]를 사용하면 this가 암묵적으로 값으로 복사되어 캡처된다.
+// - 지역 변수는 값으로 캡처함.
+// - 멤버 변수는 값으로 캡처하지 않고 암묵적으로 캡처된 this를 통해 접근함.
+// 즉, 멤버 변수를 값(value)으로 캡처할 용도로 [=]를 사용했는데 정작 this라는 숨은 포인터를 캡처해서 사용한다는 뜻이다.
+//
+// https://en.cppreference.com/w/cpp/language/lambda.html#Dangling_references
+// 이에 따라 발생 가능한 문제는 this의 dangling 포인터화이다.
+//
+// C++20 이후부터는 이러한 문법적인 혼란을 줄이기 위해 *this를 지원한다(사용은 [=, *this]가 정석임).
+// struct S2 { void f(int i); };
+// void S2::f(int i)
+// {
+//     [=] {};        // OK: by-copy capture default
+//     [=, &i] {};    // OK: by-copy capture, except i is captured by reference
+//     [=, *this] {}; // until C++17: Error: invalid syntax
+//                    // since C++17: OK: captures the enclosing S2 by copy
+//     [=, this] {};  // until C++20: Error: this when = is the default
+//                    // since C++20: OK, same as [=]
+// }
+
+// 인스턴스화된 클래스의 멤버 함수 내 람다식을 구성할 때 캡처 블록에 "="를 쓰면 this를 암묵적으로 값으로 복사해서 캡처한다.
+// 따라서 인스턴스가 유효하지 않은 상태가 되었을 때 람다를 호출하는 상황이 되었을 때 문제가 발생한다.
+// 
+// C++20부터는 객체의 멤버 함수 내에서 [=]로 캡처하는 것을 피하고, [=, *this]와 같은 형태로 사용할 것을 권장한다.
+// 물론 [=]나 [this]로 캡처하는 것도 여전히 유효하지만, C++20 이후부터 [=]를 사용할 경우 암묵적인 this 캡처가 발생한다고 컴파일러 경고가 뜰 확률이 매우 높아졌다.
 
 class FooObject
 {
@@ -38,7 +63,7 @@ public:
 
     function<void()> GetGoodLambda(int captureVal)
     {
-        // C++17부터는 *this를 이용하여 인스턴스의 값을 캡처하는 것이 가능하다.
+        // C++20부터는 *this를 이용하여 인스턴스의 값을 캡처하는 것이 가능하다.
         // *this : simple by-copy capture of the current object.
         return [=, *this] // () mutable
             {
